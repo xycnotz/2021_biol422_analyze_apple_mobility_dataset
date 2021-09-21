9901dd7a78457bc1a1fc74047a1c70e4aff54688
Xienyam Chiu <xichiu890@gmail.com>
2021-09-21 15:49:22 -0700

    First pass to tally up cities and counties with data

---
+++ code/02_count_up_counties_cities.R
@@ -0,0 +1,27 @@
+# the goal of this script is to use the dplyr package to count up the
+# number f cities and counties in a given state that have detialed
+# Apple mobility data available and then eventually to save it to csv
+
+# Xienyam Chiu
+# xchiu@dons.usfca.edu
+# September 21, 2021
+
+# load the package "dplyr"
+library("dplyr")
+
+# we would like to get a count of the number of cities and the
+# number of counties in a given state that have driving mobility
+# data. The input to this will be the output of the previous script
+# namely the state csv files that were already subsetted.
+
+# load in the dataset from the previous script
+input_file <- "output/applemobilitytrends-2021-09-13_Florida.csv"
+state_data <- read.csv(input_file)
+
+# starting off with dplyr chains
+count_of_cities_counties <- state_data %>%
+  select(geo_type, region, transportation_type) %>%
+  group_by(geo_type, transportation_type)%>%
+  tally()
+
+write.csv(count_of_cities_counties, "output/florida_cities_counties_counts.csv")
