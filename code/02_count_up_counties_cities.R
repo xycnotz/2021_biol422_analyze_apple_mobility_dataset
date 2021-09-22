9ff3f447da039d080e8d8a6aaf6ba7d5d01dccc8
Xienyam Chiu <xichiu890@gmail.com>
2021-09-21 17:06:35 -0700

    Modify second script to call function

--- code/02_count_up_counties_cities.R
+++ code/02_count_up_counties_cities.R
@@ -1,28 +1,23 @@
 # the goal of this script is to use the dplyr package to count up the
 # number f cities and counties in a given state that have detialed
 # Apple mobility data available and then eventually to save it to csv
+# Uses functions in the code/functions directory
 
 # Xienyam Chiu
 # xchiu@dons.usfca.edu
 # September 21, 2021
-
-# load the package "dplyr"
-library("dplyr")
 
 # we would like to get a count of the number of cities and the
 # number of counties in a given state that have driving mobility
 # data. The input to this will be the output of the previous script
 # namely the state csv files that were already subsetted.
 
-# load in the dataset from the previous script
-input_file <- "output/applemobilitytrends-2021-09-13_Florida.csv"
-state_data <- read.csv(input_file)
+# load functions
+source("code/functions/state_city_county_transportation_type_tally.R")
 
-# starting off with dplyr chains
-count_of_cities_counties <- state_data %>%
-  select(geo_type, region, transportation_type) %>%
-  group_by(geo_type, transportation_type) %>%
-  tally()
-
-# write out the result of the dplyr chain to csv
-write.csv(count_of_cities_counties, "output/florida_cities_counties_counts.csv")
+# call function for multiple states
+for (state in c("Florida", "Alabama",
+                "North Carolina", "Idaho",
+                "Nebraska", "New York")) {
+  state_transportation_tally(state)
+}
