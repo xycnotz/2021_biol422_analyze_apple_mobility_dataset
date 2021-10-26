16a5e3b666efe8c70923a32c549218741c8cf3a0
Xienyam Chiu <xichiu890@gmail.com>
2021-10-26 20:47:32 +0000

    Add preliminary bash functions to script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 zcat /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz | zgrep ">" | wc -l
+zcat /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz | zgrep ">" | 
+zcat /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz | zgrep ">" | cut -d "|" -f 1,3,4 |sort -g | head -n 30
