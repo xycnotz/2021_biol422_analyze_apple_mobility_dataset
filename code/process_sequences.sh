64bcdebdd93b361e07ce7342a660e567ccd2842c
Xienyam Chiu <xichiu890@gmail.com>
2021-11-08 04:42:57 +0000

    Edit sequence script to accept more arguments and add defensive code

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 # This bash script is used to calculate the total number of SARS-CoV2 sequences
 # as well as show the number of sequences isolated in each country in
 # reverse numeric order
@@ -8,23 +9,34 @@
 # compressed FASTA file
 
 # Xienyam Chiu
-# October 31, 2021
+# November 07, 2021
 # xchiu@dons.usfca.edu
 
 if [ $# -eq 0 ]
 then
-	echo "Please supply one argument to run this script"
-	echo "This script expect the path to the compressed FASTA file."
+	echo "Please supply up to two arguments to run this script"
+	echo "This script expect the path to the compressed FASTA file as the first argument."
+	echo "The second argument please enter \"ALL\" if you want a verbose log of the script."
 	exit 1
+elif [ $# -ge 3 ]
+then
+	echo "This script only accepts up two two arguments."
+	echo "Please check if you entered any extra arguments."
+	exit 1
+else
+	if [ $# -eq 2 ] && [ "$2" = "ALL" ]
+	then
+		# Count the number of SARS-CoV2 sequences
+		touch output/final_output.txt
+		echo "Beginning total SARS-CoV2 sequnce tally"
+		echo "Total SARS-CoV2 sequences: $(zgrep ">" "$1"| wc -l)"  > output/final_output.txt
+		echo "Sequence count complete"
+
+		# Count up all the SARS-CoV2 sequences by country and sort in descending order
+		echo "Beginning SARS-CoV2 sequence tally by country"
+		zgrep ">" "$1"| cut -d "|" -f 21 | sort | uniq -c | sort -nr >> output/final_output.txt
+		echo "Country sequence tally complete"
+	else
+		zgrep ">" "$1"| cut -d "|" -f 21 | sort | uniq -c | sort -nr >> output/final_output.txt
+	fi
 fi
-
-# Count the number of SARS-CoV2 sequences
-touch output/final_output.txt
-echo "Beginning total SARS-CoV2 sequnce tally"
-echo "Total SARS-CoV2 sequences: $(zgrep ">" "$1"| wc -l)"  > output/final_output.txt
-echo "Sequence count complete"
-
-# Count up all the SARS-CoV2 sequences by country and sort in descending order
-echo "Beginning SARS-CoV2 sequence tally by country"
-zgrep ">" "$1"| cut -d "|" -f 21 | sort | uniq -c | sort -nr >> output/final_output.txt
-echo "Country sequence tally complete"
